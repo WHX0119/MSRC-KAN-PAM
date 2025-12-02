9c3d9a72488689e5b188f13a1281c94ce1d7ac5a
Hongxing Wang <109292264+WHX0119@users.noreply.github.com>
2025-12-03 02:30:19 +0800

    Create __init__.py

---
+++ model/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+from models.MSRC_KAN_PAM import MSRC_KAN_PAM as MSRC_KAN_PAM
+
+from models.QCNN import QCNN as QCNN
+from models.LaplaceWaveletNet import LaplaceWaveletNet as LaplaceWaveletNet
+from models.WDCNN import WDCNN as WDCNN
+
+from models.CNN_BiLSTM import CNN_BiLSTM as CNN_BiLSTM
+from models.ResNet18 import ResNet18 as ResNet18
+from models.AlexNet import AlexNet as AlexNet
+from models.LeNet import LeNet as LeNet
